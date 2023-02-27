a1fa9afe51594d774e45482c82f91d7a1c92418a
Vladimir <user@DESKTOP-M2I6II7.localdomain>
2023-02-27 15:30:32 +1000

    refactored

--- lib/hexlet_code/form_for.rb
+++ lib/hexlet_code/form_for.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module HexletCode
-    def self.form_for(obj, options = {})
+    def self.form_for(model, options = {})
         url = options.fetch(:url, '#')
 
         form_body = if block_given?
-            form_fields = FormFields.new(obj)
+            form_fields = FormFields.new(model)
             yield(form_fields)
             form_fields.to_html
 
@@ -14,38 +14,37 @@ module HexletCode
     end
 
     class FormFields
-        def initialize(obj)
-        @fields = []
-        @obj = obj
+        def initialize(model)
+        @tags = []
+        @model = model
+
 
         end
 
 
         def input(field_name, options = {})
-            if options[:as]
-                default_values = {cols: '20', rows: '40'}
-                options = options.reject {|k,v| k == :as}
-                merged_params = default_values.merge(options)
-
-                build_line = ({name: field_name}).merge(merged_params)
-                @fields << HexletCode::Tag.build('textarea', build_line) {@obj[field_name]}
+            default_options = {cols: '20', rows: '40'}
+            if options.delete(:as)
+                merged_options = default_options.merge(options)
+                build_line = ({name: field_name}).merge(merged_options)
+                @tags << HexletCode::Tag.build('textarea', build_line) {@model[field_name]}
             else
-                @fields << HexletCode::Tag.build('input', name: field_name, type: 'text', value: @obj[field_name])
+                @tags << HexletCode::Tag.build('input', name: field_name, type: 'text', value: @model[field_name])
             end
         end
 
         def to_html
-            @fields.join
+            @tags.join
         end
 
-        def submit(value = 'Save')
-            @fields << Elements::Submit.new(value)
-        end
-
-
-        def submit(value = 'Save', **attrs)
-            @elements << Elements::InputElement.new(value, type: 'submit', **attrs)
-        end
+#        def submit(value = 'Save')
+#            @fields << Elements::Submit.new(value)
+#        end
+#
+#
+#        def submit(value = 'Save', **attrs)
+#            @elements << Elements::InputElement.new(value, type: 'submit', **attrs)
+#        end
 
    end
 
