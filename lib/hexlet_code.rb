dbf44b33675ce5f31af244d067f6a91966c03622
Kim Vladimir <kimvova85@mail.ru>
2023-07-04 10:41:09 +1000

    Fix requires

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,7 +2,7 @@
 
 require_relative 'hexlet_code/version'
 require_relative 'hexlet_code/tag'
-require_relative 'hexlet_code/form_for'
+require_relative 'hexlet_code/form_fields'
 
 module HexletCode
   class Error < StandardError; end
