a019163d8c316aa4bf5bb993e84d958986d7864f
Kim Vladimir <kimvova85@mail.ru>
2023-07-04 10:33:53 +1000

    Rename form_for to form_fields

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,5 +6,21 @@ require_relative 'hexlet_code/form_for'
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.form_for(model, form_options = {})
+    options = form_options.dup
+    options[:action] = form_options.fetch(:url, '#')
+    options[:method] = form_options.fetch(:method, 'post')
+    options.delete(:url)
+
+    form_body = if block_given?
+                  form_fields = FormFields.new(model)
+                  yield(form_fields)
+                  form_fields.to_html
+
+                end
+
+    HexletCode::Tag.build('form', options.sort.to_h) { form_body }
+  end
+
 end
--- lib/hexlet_code/form_fields.rb
+++ lib/hexlet_code/form_fields.rb
@@ -1,22 +1,6 @@
 # frozen_string_literal: true
 
 module HexletCode
-  def self.form_for(model, form_options = {})
-    options = form_options.dup
-    options[:action] = form_options.fetch(:url, '#')
-    options[:method] = form_options.fetch(:method, 'post')
-    options.delete(:url)
-
-    form_body = if block_given?
-                  form_fields = FormFields.new(model)
-                  yield(form_fields)
-                  form_fields.to_html
-
-                end
-
-    HexletCode::Tag.build('form', options.sort.to_h) { form_body }
-  end
-
   class FormFields
     def initialize(model)
       @tags = []
