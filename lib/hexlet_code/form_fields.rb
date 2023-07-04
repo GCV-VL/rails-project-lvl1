6ef937979525b4e7710038e2f4740f56ff829223
Kim Vladimir <kimvova85@mail.ru>
2023-07-04 10:41:21 +1000

    Refactor

--- lib/hexlet_code/form_fields.rb
+++ lib/hexlet_code/form_fields.rb
@@ -8,26 +8,27 @@ module HexletCode
     end
 
     def input(field_name, input_options = {})
-      input_options[:name] = field_name
+      as = input_options[:as]
+      options = input_options.except(:as)
+      options[:name] = field_name
 
       @tags << HexletCode::Tag.build('label', for: field_name) { field_name.capitalize }
 
-      if input_options.delete(:as)
-        build_textarea(field_name, input_options)
+      if as
+        build_textarea(field_name, options)
       else
-        build_input(field_name, input_options)
+        build_input(field_name, options)
       end
     end
 
     def build_textarea(field_name, input_options)
-      input_options = { cols: '20', rows: '40' }.merge(input_options)
-      @tags << HexletCode::Tag.build('textarea', input_options.sort.to_h) { @model.public_send(field_name) }
+      options = { cols: '20', rows: '40' }.merge(input_options)
+      @tags << HexletCode::Tag.build('textarea', options.sort.to_h) { @model.public_send(field_name) }
     end
 
     def build_input(field_name, input_options)
-      input_options[:type] = 'text'
-      input_options[:value] = @model.public_send(field_name)
-      @tags << HexletCode::Tag.build('input', input_options.sort.to_h)
+      options = { type: 'text', value: @model.public_send(field_name) }.merge(input_options)
+      @tags << HexletCode::Tag.build('input', options.sort.to_h)
     end
 
     def submit(value = 'Save')
