7194392da3d76dba19f0bc06fd9f7434cd5103aa
Kim Vladimir <kimvova85@mail.ru>
2023-07-17 09:37:59 +1000

    Refactor

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -5,15 +5,13 @@ module HexletCode
     SINGLE_TAGS = %w[br input img].freeze
 
     def self.build(tag, options = {})
-      body = yield if block_given?
-
       options_string = options.map do |key, value|
         " #{key}=\"#{value}\""
       end.join('')
-
       
       return "<#{tag}#{options_string}>" if SINGLE_TAGS.include? tag
 
+      body = yield if block_given?
       "<#{tag}#{options_string}>#{body}</#{tag}>"
     end
   end
